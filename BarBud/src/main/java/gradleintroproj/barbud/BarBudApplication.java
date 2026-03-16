8209978e6f21f15586c88c073adc883b4baf68bb
WojtekZb <554101@student.fontys.nl>
2026-03-16 14:55:01 +0100

    Project Setup with Springboot

---
+++ BarBud/src/main/java/gradleintroproj/barbud/BarBudApplication.java
@@ -0,0 +1,13 @@
+package gradleintroproj.barbud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarBudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BarBudApplication.class, args);
+    }
+
+}
---
+++ BarBud/src/test/java/gradleintroproj/barbud/BarBudApplicationTests.java
@@ -0,0 +1,13 @@
+package gradleintroproj.barbud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarBudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
